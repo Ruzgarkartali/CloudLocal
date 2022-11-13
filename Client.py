0a319649352fb85aa04cf696aea345fff4b43382
Ruzgarkartali <melih.taki@gmail.com>
2022-11-13 14:41:04 +0100

    dd

---
+++ Client.py
@@ -0,0 +1,17 @@
+import socket
+
+from past.builtins import raw_input
+
+
+def Main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip = '127.0.0.1'
+    port = 4500
+    address = (ip, port)
+    message = 'mymessage'
+    client = socket.socket()
+    client.connect(address)
+    client.sendall(message.encode('utf-8'))
+
+if __name__ == '__main__':
+    Main()
